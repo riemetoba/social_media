828cc5f55fedaf2ec124cde8b5c41c514e74f3e6
toba <riemehasan06@gmail.com>
2026-03-12 11:51:35 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Footer from './components/layouts/Footer'
+import Header from './components/layouts/Header'
+
+function App() {
+ 
+
+  return (
+   <>
+   <Header/>
+   <Footer/>
+   
+   </>
+  )
+}
+
+export default App
---
+++ src/components/layouts/Header.jsx
@@ -0,0 +1,16 @@
+
+
+const Header = () => {
+  return (
+   <>
+ <nav className="bg-primary">
+    <h1>
+    fdibhfbdhbf
+    </h1>
+    </nav>  
+
+ </>
+  )
+}
+
+export default Header
